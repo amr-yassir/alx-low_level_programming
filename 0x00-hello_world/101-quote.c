2a0b2a1c7a0dd03122a27afd988c3d62b4ab98b3
amr-yassir <amryassir66@gmail.com>
2023-07-16 20:22:26 +0000

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Description: A C program to print a text
- *
- * Return: Always 0 (success)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
