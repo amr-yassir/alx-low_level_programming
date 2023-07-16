4ecf7e2d3705427f7e58e71da28820b813b97781
amr-yassir <amryassir66@gmail.com>
2023-07-16 19:52:54 +0000

    Hello C task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %u bytes\n", sizeof(char));
+	printf("Size of a int: %u bytes\n", sizeof(int));
+	printf("Size of a long int: %u bytes\n", sizeof(long int));
+	printf("Size of a long long int: %u bytes\n", sizeof(long long int));
+	printf("Size of a float: %u bytes\n", sizeof(float));
+
+	return (0);
+}
