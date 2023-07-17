b60859130102ab24cfb1398672a67a31b370cb23
amr-yassir <amryassir66@gmail.com>
2023-07-17 18:12:24 +0000

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: print the alphaBET
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
