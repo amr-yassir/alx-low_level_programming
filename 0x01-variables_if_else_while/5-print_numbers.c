4a80a8ca2f92f0fc3df920d9f27b1d3df4e9b94e
amr-yassir <amryassir66@gmail.com>
2023-07-17 18:37:04 +0000

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print numbers from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	for (int i; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
