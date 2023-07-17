bf55ba49cdfd627231fc23ec9f13a9dd24148224
amr-yassir <amryassir66@gmail.com>
2023-07-17 19:56:41 +0000

    task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: print combinations
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i < 57)
+			putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
