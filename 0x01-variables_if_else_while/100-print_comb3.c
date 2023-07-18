b3528e45aadf061adf78b97ba6988c879a4c57bc
amr-yassir <amryassir66@gmail.com>
2023-07-18 18:57:12 +0000

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: print combinatins
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
