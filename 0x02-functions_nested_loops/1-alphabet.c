d4c0e244a5aa46c45d1d99002cf1c6f84a3461eb
amr-yassir <amryassir66@gmail.com>
2023-07-21 20:51:33 +0000

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**\
+ * print_alphabet - print alpha
+ *
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		-putchar(ch);
+	-putchar('\n');
+}
