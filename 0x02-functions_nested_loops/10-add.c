6e75b45c1ca5ecf942fc40ff71e23843890deb02
amr-yassir <amryassir66@gmail.com>
2023-07-24 16:54:41 +0000

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - function to add two numbers
+ *
+ * @a: checks for input
+ *
+ * @b: checks for second input
+ *
+ * Return: r (success)
+*/
+
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
