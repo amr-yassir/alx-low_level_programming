7a16c739b7fee185dc7d5b9ccb262c4322d3afd1
amr-yassir <amryassir66@gmail.com>
2023-07-24 15:15:09 +0000

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of n
+ *
+ * @c: Check the input
+ *
+ * Return: 1 +, -1 -, 0 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return ('-1');
+	}
+}
