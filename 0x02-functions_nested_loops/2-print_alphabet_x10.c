994a221e0eb6536cac815f822ece3db7b6cdb840
amr-yassir <amryassir66@gmail.com>
2023-07-24 13:31:15 +0000

    task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet x10
+*/
+
+void print_alphabet_x10(void);
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
