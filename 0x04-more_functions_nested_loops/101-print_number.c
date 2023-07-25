8505adca16d975b21de7955cf1292f53591e7859
amr-yassir <amryassir66@gmail.com>
2023-07-25 16:27:50 +0000

    task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer
+ * @n: check input
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	putchar(i % 10 + '0');
+}
