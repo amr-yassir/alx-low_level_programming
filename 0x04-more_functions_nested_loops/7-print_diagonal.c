5e09fce7de606b3a2646c6b400456dd89ada2350
amr-yassir <amryassir66@gmail.com>
2023-07-25 14:49:17 +0000

    task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,22 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	for (i = 0; i < n; i++)
+	{
+		if (n > 1)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+
 	if (n <= 0)
 	{
-		putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j <= i; j++)
-				putchar(' ');
-			putchar('\\');
-			putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
