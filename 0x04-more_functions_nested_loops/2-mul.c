e82d2cedbbfbe9d166f3849ad08184c9674ad256
amr-yassir <amryassir66@gmail.com>
2023-07-25 12:55:56 +0000

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - multiply two numbers
+ *
+ * @a: check for input
+ *
+ * @b: check for input
+ *
+ * Return: 0 (success)
+*/
+
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	printf("%d", r);
+	return (0);
+}
