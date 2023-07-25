bc22ad49e93ed3ec34d2836e34bb3302fe293f23
amr-yassir <amryassir66@gmail.com>
2023-07-25 13:23:43 +0000

    task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print n
+ *
+ * Return: 0 (success)
+*/
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
