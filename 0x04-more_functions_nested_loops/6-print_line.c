9fc6a78f31697ab94749e0bae56caac8aff70580
amr-yassir <amryassir66@gmail.com>
2023-07-25 14:07:25 +0000

    task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - function to print _
+ *
+ * Return: 0
+*/
+void print_line(int n)
+{
+	int i;
+
+	for(i = 1; i <= n; i++)
+		putchar('_');
+	putchar('\n');
+}
