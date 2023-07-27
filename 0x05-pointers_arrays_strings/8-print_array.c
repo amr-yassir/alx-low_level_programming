29e26b627eb85b2f49a1ae757d5b58b381a1b81b
amr-yassir <amryassir66@gmail.com>
2023-07-27 17:17:20 +0000

    task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of array
+ *
+ * @a: input
+ * @n: input
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n)
+			printf(", ");
+	}
+}
