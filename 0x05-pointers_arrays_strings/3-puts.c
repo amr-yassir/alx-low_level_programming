f65edf71462f6c95bae3bdd36adbeb603f3e3e56
amr-yassir <amryassir66@gmail.com>
2023-07-27 12:39:33 +0000

    task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ *
+ * @str: check input
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	for (str = 0; *str != '\0'; str++)
+		_putchar(*str);
+}
