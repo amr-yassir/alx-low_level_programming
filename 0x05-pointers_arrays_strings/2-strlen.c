581ab9d46e0490f53f78906c6a7b4be9bd429e17
amr-yassir <amryassir66@gmail.com>
2023-07-27 12:24:06 +0000

    task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - calculate length
+ *
+ * @s: check input
+ *
+ * Return: length
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+
 
 #endif
