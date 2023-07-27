1142068ca74051186c2561f3b866810a4a4d497b
amr-yassir <amryassir66@gmail.com>
2023-07-27 17:47:58 +0000

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function to copy
+ *
+ * @dest: input
+ * @src: input
+ *
+ * Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
