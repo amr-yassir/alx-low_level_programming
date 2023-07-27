73dde728e3f87f8efe6877bfa0ccaa43745c73e9
amr-yassir <amryassir66@gmail.com>
2023-07-27 15:47:09 +0000

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half
+ *
+ * @str: input
+*/
+
+void puts_half(char *str)
+{
+	int l = 0;
+	int l2;
+
+	while (str[l])
+		l++;
+	l2 = l / 2 - 1;
+
+	while (str[l2])
+	{
+		_putchar(str[l2]);
+		l2++;
+	}
+	_putchar('\n');
+}
+
