d2aedc624d018b1e3abef2d2343cee7df99887f7
amr-yassir <amryassir66@gmail.com>
2023-07-27 19:41:06 +0000

    task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - convert
+ * 
+ * @s: input
+ *
+ * Return: num
+*/
+
+int _atoi(char *s)
+{
+	int i;
+	int num;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == '-')
+		       _putchar('-');
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			num *= 10;
+			num += s[i];
+		}
+		return (num);
+		_putchar('\n');
+	}
+}
