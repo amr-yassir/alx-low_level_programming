fe52ea2bb93d16fc300428247d0cfd9f4b6e056f
amr-yassir <amryassir66@gmail.com>
2023-07-28 22:44:04 +0000

    task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - cap all ward
+ *
+ * @str: input
+ *
+ * Return: string
+*/
+
+char *cap_string(char *str)
+{
+	int i, j;
+
+	j = 0;
+	str[0] = str[0] -32;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		while (str[j] = 32)
+		{
+			str[j + 1] = str[j + 1] - 32;
+		}
+	}
+	return (str);
+}
