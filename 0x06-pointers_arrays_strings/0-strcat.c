2f4647c002878b790d3e33826606af820058debd
amr-yassir <amryassir66@gmail.com>
2023-07-28 00:04:44 +0000

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/***/
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c1;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c1 = 0; src[c1]; c1++)
+		dest[c++] = src[c1];
+
+	return (dest);
+}
