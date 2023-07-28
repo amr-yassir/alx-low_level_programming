78cc3daf2c364f4f488e2739d6113f8e8de16010
amr-yassir <amryassir66@gmail.com>
2023-07-28 23:15:39 +0000

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,22 @@
 char *cap_string(char *str)
 {
 	int i, j;
+	char c[] = {44, 59, 66, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-	j = 0;
-	i = j + 1;
-	while (str[j] = 32)
+	i = 0;
+
+	while (str[i] != '\0')
 	{
-		str[i] = str[i] - 32;
-		j++;
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+				str[i + 1] = str[i + 1] - 32;
+			j++;
+		}
+		i++;
 	}
 	return (str);
 }
