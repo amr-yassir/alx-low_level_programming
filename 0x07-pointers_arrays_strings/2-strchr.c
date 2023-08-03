3109b74c1fbe9564acb19326142180787129e7fe
amr-yassir <amryassir66@gmail.com>
2023-08-03 16:46:13 +0000

    task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - detect the first occur
+ *
+ * @s: input
+ * @c: input
+ *
+ * Return: a
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char a[];
+
+	while (s[i])
+	{
+		if (s[i] = c)
+		{
+			for (int j = 0; s[i] != '\0'; j++)
+			{
+				a[j] = s[i];
+			}
+			break;
+		}
+		i++;
+	}
+
+	return (a);
+}
