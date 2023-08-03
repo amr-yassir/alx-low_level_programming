c6582331f028ebe5cd16fb6865a14afa849389bb
amr-yassir <amryassir66@gmail.com>
2023-08-03 21:58:43 +0000

    task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - checks for similarity
+ *
+ * @s: input
+ * @accept: input
+ *
+ * Return: i
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[i] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
