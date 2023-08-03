021f5da3b19f0b7872ff47f7a049e80e6347107e
amr-yassir <amryassir66@gmail.com>
2023-08-03 23:29:20 +0000

    task

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/***/
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j = i; j++)
+		{
+			sum += a[j];
+		}
+	}
+	putchar(sum);
+	return (0);
+}
