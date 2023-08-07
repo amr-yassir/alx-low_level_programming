89f8508cbae3f3fe9273942831ca81ab81be10fd
amr-yassir <amryassir66@gmail.com>
2023-08-07 23:24:19 +0000

    task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a text is palindrome
+ *
+ * @s: input
+ *
+ * Return: int
+*/
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (check_pal(s, 0, strlength(s)));
+}
+
+/**
+ * strlength - determine the length of a string
+ * 
+ * @s: input
+ *
+ * Return: int
+*/
+
+int strlength(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + strlength(s + 1));
+}
+
+/**
+ * check_pal - checks if a string is palindrome
+ *
+ * @s: input
+ * @i: input
+ * @len: input
+ *
+ * Return: int
+*/
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + 1) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
