84bf55b095699c6afb6d404072c2563d700882c2
amr-yassir <amryassir66@gmail.com>
2023-08-08 17:15:02 +0000

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: input
+ * @argv: input
+ *
+ * Return: int
+*/
+
+int main(int argc, char* argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
