ba9ad435636d5f73d9b4c5dc3cfe10f5ef8a5eb7
amr-yassir <amryassir66@gmail.com>
2023-08-08 18:38:28 +0000

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[])
 	int i;
 	int count = 1;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	while (count < argc)
 	{
 		for (i = 0; argv[count][i] != '\0'; i++)
