bc2b55e8ccdf4e996f9013641049f9d048484157
amr-yassir <amryassir66@gmail.com>
2023-08-09 20:54:30 +0000

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_array - create an array with specific character
+ *
+ * @size: input
+ * @c: input
+ *
+ * Return: char
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ch;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ch = (char *) malloc(size * sizeof(char));
+
+	if (ch == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ch[i] = c;
+	}
+
+	return (0);
+}
