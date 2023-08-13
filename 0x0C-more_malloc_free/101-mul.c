2463431219bb14a07e883c563704ee0ec8d83045
amr-yassir <amryassir66@gmail.com>
2023-08-13 19:19:59 +0000

    task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: input
+ * @argv: input
+ * Return: int
+*/
+
+int main(int argc, int *argv[])
+{
+	int i, j;
+	unsigned int mult;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mult = atoi(argv[1] * argv[2]);
+	return (mult);
+}
