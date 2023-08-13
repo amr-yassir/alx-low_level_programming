8fc6efe5812210055f62d22308f1fdf6a210e254
amr-yassir <amryassir66@gmail.com>
2023-08-13 15:19:13 +0000

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < l1; i++)
 		c[i] = s1[i];
 	for (j = 0; j < n; j++)
-		c[i + j] = s2[j];
+	{
+		c[i] = s2[j];
+		i++;
+	}
 
+	c[i] = '\0';
 	return (c);
 }
