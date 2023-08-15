483e450a4b0c552db416239ac5a8f2a9221fed81
amr-yassir <amryassir66@gmail.com>
2023-08-15 20:05:04 +0000

    task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,6 +18,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
 	for (l1 = 0; name[l1]; l1++)
 		;
 	l1++;
