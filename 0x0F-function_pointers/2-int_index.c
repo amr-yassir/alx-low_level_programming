5a5107009669cbe753b09d683947395897c7c462
amr-yassir <amryassir66@gmail.com>
2023-08-20 22:26:07 +0000

    task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_indeex - search for integer
+ * @array: array of elements
+ * @size: size of the array
+ * @cmp: compare function
+ *
+ * Return: i
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && cmp && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+}
