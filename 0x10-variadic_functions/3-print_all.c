dfe6c96fe33647943462ef4f4ac29a86fdaa8f8e
amr-yassir <amryassir66@gmail.com>
2023-08-21 04:39:53 +0000

    task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,22 +17,31 @@ void print_all(const char * const format, ...)
 		while (arg[j])
 		{
 			if (format[i] == arg[j] && c)
-				printf(", "), break;
-			j++;
+			{
+				printf(", ");
+				break;
+			} j++;
 		}
 		switch (format[i])
 		{
 			case 'i':
-				printf("%d", va_arg(p, int)), c = 1, break;
+				printf("%d", va_arg(p, int)), c = 1;
+				break;
 			case 'f':
-				printf("%f", va_arg(p, double)), c = 1, break;
+				printf("%f", va_arg(p, double)), c = 1;
+				break;
 			case 'c':
-				printf("%c", va_arg(p, int)), c = 1, break;
+				printf("%c", va_arg(p, int)), c = 1;
+				break;
 			case 's':
 				str = va_arg(p, char *), c = 1;
 				if (!str)
-					printf("(nil)"), break;
-				printf("%s", str), break;
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
 		} i++;
 	}
 	printf("\n"), va_end(p);
