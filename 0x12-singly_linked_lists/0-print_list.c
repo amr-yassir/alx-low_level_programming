0c0889aa9df92ceaaa0c79dc42d7913b27e13f48
amr-yassir <amryassir66@gmail.com>
2023-09-02 21:16:10 +0000

    task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print elements of a list
+ * @h: points to list_t
+ *
+ * Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", h->len, h->str ? h->str : "[0] (nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
