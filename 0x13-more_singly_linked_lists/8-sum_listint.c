413f4409d913981eb62d52cdfb6592978a258c0d
amr-yassir <amryassir66@gmail.com>
2023-09-05 07:11:56 +0000

    task

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -12,12 +12,11 @@ int sum_listint(listint_t *head)
 	listint_t *temp = head;
 	int sum = 0;
 
-	if (!temp)
-		return (0);
 	while (temp)
 	{
-		temp = temp->next;
 		sum += temp->n;
+		temp = temp->next;
 	}
+
 	return (sum);
 }
