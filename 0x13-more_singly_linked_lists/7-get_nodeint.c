58341c6085bea49aaae51d37f8bf2c73490eb7e7
amr-yassir <amryassir66@gmail.com>
2023-09-05 06:47:50 +0000

    task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i;
 
-	for (i = 0; i <= index; i++)
+	for (i = 0; i < index; i++)
 		temp = head->next;
 
 	return (temp ? temp : NULL);
