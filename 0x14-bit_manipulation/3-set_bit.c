d6d5e22d630b2d667a8e5cbfa93939fed6db055c
amr-yassir <amryassir66@gmail.com>
2023-09-07 03:41:23 +0000

    task

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,5 +12,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 
-	return (!!(*n |= 1 << index));
+	return (!!(*n |= 1L << index));
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,5 +12,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 64)
 		return (-1);
 
-	return (!!(*n &= ~(1 << index));
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+
+	return (1);
 }
