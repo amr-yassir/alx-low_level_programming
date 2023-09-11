3b7380894ad3b74700bca2a460673d06fe3028ae
amr-yassir <amryassir66@gmail.com>
2023-09-11 20:56:34 +0000

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
-/***/
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters to read.
+ *
+ * Return: The actual number of letters read and print
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int file;
     ssize_t r, w;
-    char *buffer;
+    char buffer[1024 * 8];
 
     if (!filename || !letters)
-        return (0);
-
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
         return (0);
 
     file = open(filename, O_RDONLY);
     if (file == -1)
         return (0);
-    r = read(file, buffer, letters);
-    w = write(STDOUT_FILENO, buffer, r);
-    free(buffer);
-    close(file);
 
+    r = read(file, &buf[0], letters);
+    w = write(STDOUT_FILENO, &buf[0], r);
+    close(file);
     return (w);
 }
