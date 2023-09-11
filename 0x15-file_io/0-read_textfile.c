88220cff068de3a41c2ccf4f10564058ca2e61ef
amr-yassir <amryassir66@gmail.com>
2023-09-11 20:13:07 +0000

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     char text[letters];
 
     file = fopen(filename, "r");
-    if (!file | !filename)
+    if (!file || !filename)
         return (0);
     while (fgets(file, letters, text))
         printf("%s", text);
