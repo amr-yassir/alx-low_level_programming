f97486b0be6a6688d11f64bbf0556ed4dd17f5ca
amr-yassir <amryassir66@gmail.com>
2023-09-11 20:37:00 +0000

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,22 +4,18 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *file;
-    char text[letters];
-    __ssize_t w;
+    int file;
+    ssize_t r, w;
+    char text[1024 * 8];
 
     if (!filename || !letters)
         return (0);
-
-    file = fopen(filename, "r");
-    if (!file)
+    file = open(filename, O_RDONLY);
+    if (file == -1)
         return (0);
-    else
-    {
-        while (fgets(text, letters, file))
-        {
-            w = write(1, &text, 1);
-        }
-    }
-    return(w);
+    r = read(file, &text[0], letters);
+    w = write(STDOUT_FILENO, &text[0], r);
+    close(file);
+
+    return (w);
 }
