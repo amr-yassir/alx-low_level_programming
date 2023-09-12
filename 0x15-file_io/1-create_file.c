acd03fba0209979c4e09b3d85e35b9df3ac0f305
amr-yassir <amryassir66@gmail.com>
2023-09-12 14:58:03 +0000

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,9 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	while(*text_content++)
-		len++;
-
-	w = write(file, text_content, len);
+		w = write(file, text_content, len);
 	if (w == -1)
 		return (-1);
 
