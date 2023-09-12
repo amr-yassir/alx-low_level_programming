051fc78ba56109bc2267ae6e9153884f26384547
amr-yassir <amryassir66@gmail.com>
2023-09-12 18:33:32 +0000

    task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,10 +14,7 @@ int main(int argc, char *argv[])
 	int cp_from, cp_to, rd, wr;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	cp_from = open(argv[1], O_RDONLY);
 	if (cp_from == -1)
 	{
