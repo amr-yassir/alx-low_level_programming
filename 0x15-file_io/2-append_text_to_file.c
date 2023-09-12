b4eb7270fb9676294bc0798c93b2c989adc50c1a
amr-yassir <amryassir66@gmail.com>
2023-09-12 15:36:51 +0000

    task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,9 +16,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
 	while (text_content[len] != '\0')
 		len++;
 
@@ -27,11 +24,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	w = write(filename, text_content, len);
-	if (w == -1)
-	{
-		close(file);
-		return (-1);
-	}
 
 	close(file);
 
