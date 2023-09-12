8dcbb9bc60ce4422aaf75ccf936c23daffee4856
amr-yassir <amryassir66@gmail.com>
2023-09-12 18:06:57 +0000

    task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	char buffer[BUF_SIZE];
+	int cp_from, cp_to, rd, wr;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cp_from = open(argv[1], O_RDONLY);
+	if (cp_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	rd = read(cp_from, buffer, BUF_SIZE);
+	if (rd == -1)
+	{
+                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+                exit(98);
+	}
+
+	cp_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (cp_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while (rd > 0)
+		wr = write(cp_to, buffer, rd);
+	if (wr == -1)
+	{
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+                exit(98);
+	}
+
+	cp_from = close(cp_from);
+	cp_to = close(cp_to);
+
+	if (cp_from)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_from);
+		exit(100);
+	}
+	if (cp_to)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_to);
+		exit(100);
+	}
+
+	return (0);
+}
