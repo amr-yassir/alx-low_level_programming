43ff1436d86ea11e805c719ec251714afda266f4
amr-yassir <amryassir66@gmail.com>
2023-09-12 14:09:22 +0000

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t fd, r, w;
+	ssize_t file, r, w;
 
 	if (!filename || !letters)
 		return (0);
@@ -20,18 +20,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buffer)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	r = read(fd, buffer, letters);
+	file = open(filename, O_RDONLY);
+	r = read(file, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (w != r || fd == -1 || r == -1 || w == -1)
+	if (w != r || file == -1 || r == -1 || w == -1)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(fd);
+	close(file);
 
 	return (w);
 }
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Pointer to the file
+ * @text_content: Pointer to a string
+ *
+ * Return: -1 if failed, 1 otherwise
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, w;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(file, text_content++, 1);
+
+	if (!file || !w)
+		return (-1);
+	close(file);
+
+	return (1);
+}
