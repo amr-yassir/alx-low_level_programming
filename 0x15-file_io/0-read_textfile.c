edc39c1fdfd36ec0d19f0673cfcd82feace72e39
amr-yassir <amryassir66@gmail.com>
2023-09-12 00:22:12 +0000

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file;
-    ssize_t r;
-    char buffer[READ_BUF_SIZE * 8];
+    char *buffer;
+    ssize_t fd, r, w;
 
     if (!filename || !letters)
         return (0);
-    file = open(filename, O_RDONLY);
-    if (file == -1)
+
+    buffer = malloc(sizeof(char) * letters);
+    if (!buffer)
         return (0);
-    r = read(file, &buffer[0], letters);
-    r = write(STDOUT_FILENO, &buffer[0], r);
-    close(file);
-    return (r);
+
+    fd = open(filename, O_RDONLY);
+    r = read(fd, buffer, letters);
+    w = write(STDOUT_FILENO, buffer, r);
+
+    if (w != r || fd == -1 || r == -1 || w == -1)
+    {
+        free(buffer);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+
+    return (w);
 }
