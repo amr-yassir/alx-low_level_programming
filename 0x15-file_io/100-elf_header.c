87f499fded4c146de9aa51e31bab304d0d1bd4bc
amr-yassir <amryassir66@gmail.com>
2023-09-13 01:28:29 +0000

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,14 +1,5 @@
 #include "main.h"
 
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char e_ident[EI_NIDENT]);
-void print_data(unsigned char e_ident[EI_NIDENT]);
-void print_version(unsigned char e_ident[EI_NIDENT]);
-void print_osabi(unsigned char e_ident[EI_NIDENT]);
-void print_abiversion(unsigned char e_ident[EI_NIDENT]);
-void print_type(Elf64_Ehdr ehdr);
-void print_entry(Elf64_Ehdr ehdr);
-
 /**
  * main - Displays the information contained in the ELF header
  * @argc: Number of arguments
@@ -17,39 +8,6 @@ void print_entry(Elf64_Ehdr ehdr);
  * Return: 1 on success, 0 on failure
  *
 */
-
-int main(int argc, char *argv[])
-{
-	Elf64_Ehdr h;
-
-	if (argc != 2)
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
-	int file = open(argv[1], O_RDONLY);
-	if (file == -1)
-		dprintf(STDERR_FILENO, "Can't open file: %s\n", argv[1]), exit(98);
-	ssize_t rd = read(file, &h, sizeof(h));
-	if (rd != sizeof(h) || rd < 1)
-		dprintf(STDERR_FILENO, "Can't read from file: %s\n", argv[1]), exit(98);
-	if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' && h.e_ident[2] == 'L' &&
-			h.e_ident[3] == 'F')
-		printf("ELF Header:\n");
-	else
-		dprintf(STDERR_FILENO, "Not ELF file: %s\n", argv[1]), exit(98);
-
-	print_magic(h.e_ident);
-	print_class(h.e_ident);
-	print_data(h.e_ident);
-	print_version(h.e_ident);
-	print_osabi(h.e_ident);
-	print_abiversion(h.e_ident);
-	print_type(h);
-	print_entry(h);
-
-	if (close(file) == -1)
-		dprintf(STDERR_FILENO, "Error closing file descriptor: %d\n", file), exit(98);
-
-	return (EXIT_SUCCESS);
-}
 
 void print_magic(unsigned char *e_ident)
 {
@@ -150,4 +108,37 @@ void print_type(Elf64_Ehdr ehdr)
 void print_entry(Elf64_Ehdr ehdr)
 {
 	printf("  Entry point address:               0x%lx\n", (unsigned long)ehdr.e_entry);
+}
+
+int main(int argc, char *argv[])
+{
+	Elf64_Ehdr h;
+
+	if (argc != 2)
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+	int file = open(argv[1], O_RDONLY);
+	if (file == -1)
+		dprintf(STDERR_FILENO, "Can't open file: %s\n", argv[1]), exit(98);
+	ssize_t rd = read(file, &h, sizeof(h));
+	if (rd != sizeof(h) || rd < 1)
+		dprintf(STDERR_FILENO, "Can't read from file: %s\n", argv[1]), exit(98);
+	if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' && h.e_ident[2] == 'L' &&
+			h.e_ident[3] == 'F')
+		printf("ELF Header:\n");
+	else
+		dprintf(STDERR_FILENO, "Not ELF file: %s\n", argv[1]), exit(98);
+
+	print_magic(h.e_ident);
+	print_class(h.e_ident);
+	print_data(h.e_ident);
+	print_version(h.e_ident);
+	print_osabi(h.e_ident);
+	print_abiversion(h.e_ident);
+	print_type(h);
+	print_entry(h);
+
+	if (close(file) == -1)
+		dprintf(STDERR_FILENO, "Error closing file descriptor: %d\n", file), exit(98);
+
+	return (EXIT_SUCCESS);
 }
