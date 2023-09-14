b952f381618e4148e02cb804c081dd762c299fe2
amr-yassir <amryassir66@gmail.com>
2023-09-14 20:04:35 +0000

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -80,9 +80,11 @@ void check_elf(unsigned char *e_ident)
 
 void print_magic(unsigned char *e_ident)
 {
+	int index;
+
 	printf("  Magic:   ");
 
-	for (int index = 0; index < EI_NIDENT; index++)
+	for (index = 0; index < EI_NIDENT; index++)
 	{
 		printf("%02x", e_ident[index]);
 
