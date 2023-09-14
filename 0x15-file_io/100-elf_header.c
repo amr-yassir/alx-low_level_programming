c1b161b228af78698aa626d530d45e3944252130
amr-yassir <amryassir66@gmail.com>
2023-09-14 13:32:30 +0000

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,112 +5,11 @@
  * @argc: Number of arguments
  * @argv: Array of pointers to the arguments
  *
- * Return: 1 on success, 0 on failure
+ * Return: 1 on success
  *
 */
 
-void print_magic(unsigned char *e_ident)
-{
-	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", e_ident[i]);
-	}
-	printf("\n");
-}
-
-void print_class(unsigned char e_ident[EI_NIDENT])
-{
-	printf("  Class:                             ");
-	switch (e_ident[EI_CLASS])
-	{
-		case ELFCLASS32:
-			printf("ELF32\n");
-			break;
-		case ELFCLASS64:
-			printf("ELF64\n");
-			break;
-		default:
-			printf("Invalid\n");
-	}
-}
-
-void print_data(unsigned char e_ident[EI_NIDENT])
-{
-	printf("  Data:                              ");
-	switch (e_ident[EI_DATA])
-	{
-		case ELFDATA2LSB:
-			printf("2's complement, little endian\n");
-			break;
-		case ELFDATA2MSB:
-			printf("2's complement, big endian\n");
-			break;
-		default:
-			printf("Invalid\n");
-	}
-}
-
-void print_version(unsigned char e_ident[EI_NIDENT])
-{
-	printf("  Version:                           0x%x (current)\n", e_ident[EI_VERSION]);
-}
-
-void print_osabi(unsigned char e_ident[EI_NIDENT])
-{
-	printf("  OS/ABI:                            ");
-	switch (e_ident[EI_OSABI])
-	{
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V\n");
-			break;
-		case ELFOSABI_HPUX:
-			printf("UNIX - HP-UX\n");
-			break;
-		case ELFOSABI_STANDALONE:
-			printf("Standalone App\n");
-			break;
-		default:
-			printf("Other\n");
-	}
-}
-
-void print_abiversion(unsigned char e_ident[EI_NIDENT])
-{
-	printf("  ABI Version:                       %d\n", e_ident[EI_ABIVERSION]);
-}
-
-void print_type(Elf64_Ehdr h)
-{
-	printf("  Type:                              ");
-	switch (h.e_type)
-	{
-		case ET_NONE:
-			printf("NONE (Unknown)\n");
-			break;
-		case ET_REL:
-			printf("REL (Relocatable file)\n");
-			break;
-		case ET_EXEC:
-			printf("EXEC (Executable file)\n");
-			break;
-		case ET_DYN:
-			printf("DYN (Shared object file)\n");
-			break;
-		case ET_CORE:
-			printf("CORE (Core file)\n");
-			break;
-		default:
-			printf("Other\n");
-	}
-}
-
-void print_entry(Elf64_Ehdr h)
-{
-	printf("  Entry point address:               0x%lx\n", (unsigned long)h.e_entry);
-}
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	Elf64_Ehdr h;
 
@@ -140,5 +39,5 @@ int main(int argc, char *argv[])
 	if (close(file) == -1)
 		dprintf(STDERR_FILENO, "Error closing file descriptor: %d\n", file), exit(98);
 
-	return (EXIT_SUCCESS);
+	return (1);
 }
