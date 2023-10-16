7a21736aecba3d1417083db3e279b7c094dd411b
amr-yassir <amryassir66@gmail.com>
2023-10-16 20:16:37 +0000

    task

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
-/***/
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to the list
+*/
 void free_dlistint(dlistint_t *head)
 {
 	while (head)
 	{
+		free(head);
 		head = head->next;
-		free(head)
 	}
 }
