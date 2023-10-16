0e3a9773f105a7927ae252ed402351254f024628
amr-yassir <amryassir66@gmail.com>
2023-10-16 21:19:11 +0000

    task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
-			new->n = n
+			new->n = n;
 			new->prev = current;
 			new->next = current->next;
 			current->next->prev = new;
