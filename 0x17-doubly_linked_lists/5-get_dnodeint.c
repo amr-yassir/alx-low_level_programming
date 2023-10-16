3747ce16fab396de1f123cc9a1c7d1c0eb6f5101
amr-yassir <amryassir66@gmail.com>
2023-10-16 20:41:15 +0000

    task

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,8 +6,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	int i;
 
-	if (!head || !index)
-		return NULL;
 	for (i = 0; i < index; i++)
 		current = current->next;
 	return (current);
